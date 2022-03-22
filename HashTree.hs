809b045a299ceb97e318bd2189f0a6e540fa119c
Mateusz <matiutd888@gmail.com>
2022-03-22 23:53:30 +0100

    Implement drawTree

--- HashTree.hs
+++ HashTree.hs
@@ -1,8 +1,10 @@
 module HashTree where
-import Hashable32 (Hash, Hashable (hash))
+import Hashable32 (Hash, Hashable (hash), showHash)
+import Data.Tree (drawTree)
 
 
-data Tree a = Leaf {treeHash::Hash, value::a} 
+data Tree a =
+            Leaf {treeHash::Hash, value::a}
             | Node {treeHash::Hash, left::Tree a, right::Tree a}
             | Twig {treeHash::Hash, son::Tree a}
 
@@ -13,9 +15,23 @@ twig :: Hashable a => Tree a -> Tree a
 twig t = Twig (hash (h, h)) t where h = treeHash t
 
 node :: Hashable a =>  Tree a -> Tree a -> Tree a
-node left right = Node (hash ((treeHash left), (treeHash right)) left right
+node left right = Node (hash (treeHash left, treeHash right)) left right
+
+buildTree :: Hashable a => [a] -> Tree a
+buildTree = buildTreeFromTree . map leaf
+    where
+        buildTreeFromTree :: Hashable a => [Tree a] -> Tree a
+        buildTreeFromTree [x] = x
+        buildTreeFromTree x = buildTreeFromTree (reverse (compress [] x))
+        compress :: Hashable a => [Tree a] -> [Tree a] -> [Tree a]
+        compress r [] = r
+        compress r [x] = twig x : r
+        compress r (x:xs) = compress (node x (head xs) : r) $ tail xs
 
 
--- buildTree :: Hashable a => [a] -> Tree a
--- treeHash :: Tree a -> Hash
--- drawTree :: Show a => Tree a -> String
+drawTree :: Show a => Tree a -> String
+drawTree = drawTreeHelp "  " where
+    drawTreeHelp :: Show a => String -> Tree a -> String
+    drawTreeHelp indent (Leaf h a) = indent ++ " " ++ showHash h ++ " " ++ show a ++ "\n"
+    drawTreeHelp indent (Twig h x) = indent ++ " " ++ showHash h  ++ " - \n" ++ drawTreeHelp ("  " ++ indent) x
+    drawTreeHelp indent (Node h l r) = indent ++ " " ++ showHash h ++ " + \n" ++ drawTreeHelp ("  " ++ indent) l ++ drawTreeHelp ("  " ++indent) r
